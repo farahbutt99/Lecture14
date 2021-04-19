88e566d4e36c2eb9b683b947f2d8a885b75a20b6
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-04-19 05:56:00 +0500

    Alert Dialog

--- Dialog/app/src/main/java/com/example/lecture14_dialog/MainActivity.java
+++ Dialog/app/src/main/java/com/example/lecture14_dialog/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.lecture14_dialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button =findViewById(R.id.btnDialog);
     }
+
+    public void ShowDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Alert Dialog");
+        builder.setMessage("It is my Alert Dialog Box");
+
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Positive",
+                new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Positive Button is clicked",Toast.LENGTH_LONG);
+            }
+        });
+        builder.setNegativeButton("Negative",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Negative Button is clicked",Toast.LENGTH_LONG);
+                    }
+                });
+        AlertDialog alertDialog= builder.create();
+        alertDialog.show();
+    }
+
 }
